8ba7b571400137499f8a97789edfb2685f72e4c0
Jonathan.Wage <Jonathan.Wage@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2007-04-11 22:07:30 +0000

    [wiki:sfGuardDoctrinePlugin]: Added method for getting user by username or e-mail address.
    git-svn-id: http://svn.symfony-project.com/plugins/sfGuardDoctrinePlugin@3765 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- lib/model/doctrine/PluginsfGuardUserTable.class.php
+++ lib/model/doctrine/PluginsfGuardUserTable.class.php
@@ -5,8 +5,17 @@
  */
 class PluginsfGuardUserTable extends Doctrine_Table
 {
+	  # should this be a static function?
     public function retrieveByUsername( $username, $isActive = true )
     {
         return $this->createQuery()->where( 'sfGuardUser.username = ? AND sfGuardUser.is_active = ?', array( $username, $isActive ) )->execute()->getFirst();
     }
+
+		static function retrieveByUsernameOrEmailAddress($usernameOrEmail)
+		{
+			$query = new Doctrine_Query();
+			$query->from('sfGuardUser u')->where('u.username = ? OR u.email_address = ?', array($usernameOrEmail, $usernameOrEmail));
+
+			return $query->limit(1)->execute()->getFirst();
+		}
 }
